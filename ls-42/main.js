31b7d8c300f201e7e73d8241c0cd7742fac3f55a
Feijhoaaaaa <67435302+Feijhoaaaaa@users.noreply.github.com>
2021-05-17 18:22:12 +0300

    Add files via upload

---
+++ ls-42/main.js
@@ -0,0 +1,56 @@
+// let a = 0 
+// for(a ;a<=100; a++){
+//     console.log(a)
+// }
+
+// let a = 100 
+// for(a ;a>=0; a--){
+//     console.log(a)
+// }
+
+// let a = 0 
+// for(a ;a<=100; a++){
+//     if(a % 2 ==0){
+//         console.log(a)
+//     }
+// }
+
+// let a = 100 
+// for(a ;a>=0; a--){
+//     if(a % 2 == 0){
+//         console.log(a)
+//     }
+// }
+
+// let a = []
+// for(i = 0;i < 10; i ++){
+//     a[i] = "x"
+// }
+// console.log(a)
+
+
+// let a = []
+// for(i = 1;i < 11; i ++){
+//     a.push(i)
+// }
+// console.log(a)
+
+// let a = []
+// for(i = 0; i < 10; i++){
+//     a.push(Math.random(0, 1) .toFixed(1))
+// }
+// console.log(a)
+
+// let a = [32, 52, 55 ,12, 98 ,54 ,45, 75]
+// for(i = 0 ;i < 8;i++){
+//     if(a[i] == 55){
+//         console.log("ya")
+//     }
+// }
+
+// let a = [1,2,3,4,5,6,7,8,9,10]
+//     b = 0
+// for(i=0 ; i<10;i++){
+//     b+=a[i]
+// }    
+// console.log(b)
---
+++ ls-44/main.js
@@ -0,0 +1,47 @@
+// let a = 0
+// function sM() {
+//     console.log("Sheeeeeeeesh")
+// }
+// sM()
+
+// function sM() {
+//     let a = "Hi World"
+//     console.log(a)
+// }
+// sM()
+
+// function sM() {
+//     let a = prompt("...")
+//     console.log(a)
+    
+// }
+// sM()
+
+// let a = prompt("You name:")
+// function MyName() {
+//     let b = "Hi"
+//     console.log(b + " " + a)
+// }
+// MyName()
+
+// function sM(from, text) {
+//     console.log(from + "poluchit" + text)    
+// }
+// sM("a ", " b")
+
+// function sM(a, b) {
+//     console.log(a + " nepoluchit " + b)    
+// }
+// let a = "a"
+// let b = "b" 
+// sM(a, b)
+
+// let a = Number(prompt("a:"))
+//     b = Number(prompt("b:"))
+    
+// function sum(a , b) {
+//     return a + b
+
+// }
+// let c = sum(a , b)
+// console.log(c)
